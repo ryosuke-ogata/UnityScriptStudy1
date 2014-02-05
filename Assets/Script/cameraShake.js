c12b8eb97bf44ab66226b131fe7763c46fb9d91e
ryosuke-ogata <ryousuke.ogata@gmail.com>
2014-02-06 03:21:12 +0900

    初回ファイル追加

---
+++ Assets/Script/cameraShake.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var moveX:float;
+private var myCamera : Camera;
+private var isCameraMove:int = 0;
+
+function Awake() {
+}
+
+function Start () {
+
+}
+
+function Update () {
+	this.gameObject.transform.position.z = 0; 
+}
+
+function OnCollisionEnter(collision : Collision) {
+	isCameraMove = 1;
+}
---
+++ Assets/Script/gravity.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var gravity:float = 0;
+
+function Awake() {
+	// Validate Input parameters
+	if (gravity <= 0) {
+		gravity = 1;
+	}
+} 
+
+function Start () {
+	//
+}
+
+function Update () {
+	this.gameObject.transform.Translate(0, -gravity * Time.deltaTime, 0);
+}
